170948540dd94921476267fd6f7eb2d4df0bea70
Sivagopal <siva@votigo.com>
2018-01-17 15:20:16 +0530

    lesson 16

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,22 @@
 console.log("Starting notes.js");
+const fs = require("fs");
 var addNote = (title, body) => {
-	console.log("Adding note ", title, body );
+	var notes = [];
+	var note = {
+		title : title,
+		body : body
+	}
+	try {
+		var notes = JSON.parse(fs.readFileSync('notes-data.json'));
+	} catch(e) {
+
+	}
+	
+	var duplicateNotes = notes.filter((note) => note.title == title)
+	if(duplicateNotes.length == 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+	}
 }
 
 var getAll = () => {
