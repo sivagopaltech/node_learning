6cad1e2341b0e0d579f77ad5d91b4f129cc62272
sivagopal <sivagopaltech@gmail.com>
2018-01-17 06:03:11 +0530

    lesson 12

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,20 +1,10 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const os = require("os");
-const notes = require("./notes.js");
 const _ = require("lodash");
 
-console.log(_.isString(true));
-console.log(_.isString("siva"));
-var filteredArray = _.uniq([1, 2, 3, 4, 1, 7, 1, 6]);
-console.log(filteredArray);
+const notes = require("./notes.js");
 
-// console.log("Result:"+ notes.add(9, -2));
-// var user = os.userInfo();
 
-// fs.appendFile("greetings.txt", `Hello ${user.username}! You are ${notes.age}`, function(err){
-// 	if(err){
-// 		console.log("Unable to write to file!");
-// 	}
-// })
+
+
