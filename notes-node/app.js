e1aa5927e6860526032de5b716da5bb6011e3264
sivagopal <sivagopaltech@gmail.com>
2018-01-17 06:20:37 +0530

    lesson 13

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,6 +5,20 @@ const _ = require("lodash");
 
 const notes = require("./notes.js");
 
+var command = process.argv[2];
+console.log("command ", command);
+
+if(command == "add") {
+    console.log("Adding new note");
+} else if(command == "list") {
+    console.log("Listing all notes");
+} else if(command == "red") {
+    console.log("Reading note");
+} else if(command == "remove") {
+    console.log("Removing note");
+} else {
+    console.log("Command not recognized");
+}
 
 
 
