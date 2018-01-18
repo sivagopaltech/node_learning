3fd071c15ab67dd3b115a06608016b3d71c79e73
sivagopal <sivagopaltech@gmail.com>
2018-01-19 00:07:17 +0530

    lesson 30

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,6 @@ request({
     url: "http://maps.googleapis.com/maps/api/geocode/json?address=gayatrinagar%20hyderabad",
     json: true
 }, (error, response, body) =>{
-    console.log(body);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 }); 
