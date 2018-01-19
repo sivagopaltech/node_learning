7b63ffa050cc2257c1c414638eabc22d01955261
Sivagopal <siva@votigo.com>
2018-01-19 15:07:02 +0530

    lesson 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,6 +14,13 @@ request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	if(error) {
+		console.log("unable to connect to google server.");
+	} else if(body.status === "ZERO_RESULTS") {
+		console.log("unable to find address.");
+	} else if(body.status === "OK") {
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
+    
 }); 
