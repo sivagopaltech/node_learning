355843ed8b2dec5785674d6b529c17d0f7baa4fc
Sivagopal <siva@votigo.com>
2018-01-19 14:35:47 +0530

    lesson 31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,17 @@
 const request = require("request");
+const yargs = require("yargs");
 
+const argv = yargs.options({
+	a :{
+		demand: true,
+		alias: 'address',
+		describe: "Address of the location",
+		string: true
+	}
+}).help().alias('help', 'h').argv;
+var encodedAddress = encodeURIComponent(argv['a'])
 request({
-    url: "http://maps.googleapis.com/maps/api/geocode/json?address=gayatrinagar%20hyderabad",
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) =>{
     console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
