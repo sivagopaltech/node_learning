dd7a5152949e24a1f4ac771ba07b34860b17fb6f
sivagopal <sivagopaltech@gmail.com>
2018-01-20 16:15:50 +0530

    lesson 33

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,24 @@
-const request = require("request");
+const yargs = require('yargs');
 
-request({
-    url: "http://maps.googleapis.com/maps/api/geocode/json?address=gayatrinagar%20hyderabad",
-    json: true
-}, (error, response, body) =>{
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-}); 
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
+});
