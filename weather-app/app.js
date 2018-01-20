9987b5c836acfc8bd023a455f9812735980356b9
sivagopal <sivagopaltech@gmail.com>
2018-01-20 16:41:24 +0530

    lesson 44

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,10 +15,22 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+const request = require("request");
+request({
+    url: "https://api.darksky.net/forecast/a46eff3ada73b7f3dbd490c2ec0d0710/17.454467,78.4034188",
+    json: true
+ }, (error, response, body) => {
+     if(!error && response.statusCode == 200) {
+        console.log(body.currently.temperature);
+     } else {
+        console.log("unable to fetch weather");
+     }
+})
