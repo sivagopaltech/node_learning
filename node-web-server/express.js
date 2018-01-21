f65f6561b81c6134e3ae1e3f55576e3d55492d28
sivagopal <sivagopaltech@gmail.com>
2018-01-21 06:37:56 +0530

    lesson 43

--- node-web-server/express.js
+++ node-web-server/express.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const hbs = require("hbs");
 
 var app = express();
+app.set("view engine","hbs");
 
 app.use(express.static(__dirname+"/public/"));
 app.get("/", (req, res) => {
     // res.send("<h1>Hello express</h1>");
-    res.send({
-        name: "siva",
-        age:28
+    res.render('home.hbs', {
+        pageTitle: "Welcome Page",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Welcome to home page"
     })
 });
 
 app.get("/about", (req, res) => {
-    res.send("Abot page");
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get("/bad", (req, res) => {
