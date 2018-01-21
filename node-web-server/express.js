5a843bdbb8f3f2b036789a347a1555c45c1d039e
sivagopal <sivagopaltech@gmail.com>
2018-01-21 15:58:25 +0530

    lesson 47

--- node-web-server/express.js
+++ node-web-server/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require("hbs");
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 app.set("view engine","hbs");
 hbs.registerPartials(__dirname+"/views/partials")
@@ -44,6 +45,6 @@ app.get("/about", (req, res) => {
 app.get("/bad", (req, res) => {
     res.send("Page not found!");
 });
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
