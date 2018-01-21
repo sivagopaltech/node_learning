441d45e45b201f588c93385a6cf5f8683231ee07
sivagopal <sivagopaltech@gmail.com>
2018-01-21 06:17:03 +0530

    lesson 42

--- node-web-server/express.js
+++ node-web-server/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname+"/public/"));
 app.get("/", (req, res) => {
     // res.send("<h1>Hello express</h1>");
     res.send({
@@ -17,4 +18,6 @@ app.get("/about", (req, res) => {
 app.get("/bad", (req, res) => {
     res.send("Page not found!");
 });
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
