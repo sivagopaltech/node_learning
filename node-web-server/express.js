ec9acc05519eba3a4d25bc58fd92d6f03fb2c419
sivagopal <sivagopaltech@gmail.com>
2018-01-21 06:06:55 +0530

    lesson 41

---
+++ node-web-server/express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+var app = express();
+
+app.get("/", (req, res) => {
+    // res.send("<h1>Hello express</h1>");
+    res.send({
+        name: "siva",
+        age:28
+    })
+});
+
+app.get("/about", (req, res) => {
+    res.send("Abot page");
+});
+
+app.get("/bad", (req, res) => {
+    res.send("Page not found!");
+});
+app.listen(3000);
