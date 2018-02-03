405619872008b99e4994a4b2ddc9f55fe9fd323b
sivagopal <sivagopaltech@gmail.com>
2018-02-03 23:55:53 +0530

    lesson 110

--- node-chat/server/server.js
+++ node-chat/server/server.js
@@ -13,14 +13,30 @@ app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
     console.log("New connection");
+    socket.emit('newMessage', {
+        from: "admin",
+        message: "Welcone to the chat app"
+    }); 
 
-    
+    socket.broadcast.emit('newMessage', {
+        from: "admin",
+        text: "New user joined the chat",
+        createdAt: new Date().toString()
+    });
 
     socket.on("createMessage", (message) => {
         console.log("create message", message);
-        io.emit('newMessage', {
-            from: "a@a.com",
-            text: "hello",
+        
+
+        // io.emit('newMessage', {
+        //     from: "a@a.com",
+        //     text: "hello",
+        //     createdAt: new Date().toString()
+        // });
+
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
             createdAt: new Date().toString()
         });
     });
